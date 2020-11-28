fc1391e85117293e94707d33f8c4c5581f8a6c4c
AlyonaDovgaya <55229453+AlyonaDovgaya@users.noreply.github.com>
2020-11-28 23:26:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Нули в дате
+function zero_first_format(value) {
+    if (value < 10) {value='0'+value;}
+    return value;
+}
+// Вывод даты
+function date_time() {
+    var current_datetime = new Date();
+    return zero_first_format(current_datetime.getDate()) + "." + zero_first_format(current_datetime.getMonth()+1) + "." + (current_datetime.getFullYear());
+}
+//Смена стиля
+function styleNew(styleNew) {
+    document.getElementById('table-style').setAttribute('href', styleNew);
+}
+
+document.getElementById("date").innerHTML = date_time(); //дата
+document.getElementById("version").innerHTML = "v0.07"; //версия сайта
+document.getElementById("name").innerHTML = "Copyright &copy Dovgaya Alyona"; //Автор
